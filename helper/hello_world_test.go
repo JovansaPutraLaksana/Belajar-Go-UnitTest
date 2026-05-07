29748757624dc6b9e473bdc35af7057a3d293a9e
Jovansa Putra Laksana <jovansaputralaksana@gmail.com>
2026-05-07 17:46:31 +0700

    finish

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -7,6 +7,61 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func BenchmarkHelloWorldWithTable(b *testing.B) {
+	benchmarks := []struct {
+		name    string
+		request string
+	}{
+		{name: "Benchmark with Jovansa", request: "Jovansa"},
+		{name: "Benchmark with empty", request: ""},
+	}
+
+	for _, benchmark := range benchmarks {
+		b.Run(benchmark.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				HelloWorld(benchmark.request)
+			}
+		})
+	}
+}
+
+func BenchmarkHelloWorldWithSubTest(b *testing.B) {
+	b.Run("Benchmark with Jovansa", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			HelloWorld("Jovansa")
+		}
+	})
+	b.Run("Benchmark with empty", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			HelloWorld("")
+		}
+	})
+}
+
+func BenchmarkHelloWorld(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		HelloWorld("Jovansa")
+	}
+}
+
+func TestHelloWorldTable(t *testing.T) {
+	tests := []struct {
+		name     string
+		request  string
+		expected string
+	}{
+		{name: "Jovansa", request: "Jovansa", expected: "Hello, Jovansa!"},
+		{name: "Empty", request: "", expected: "Hello, !"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := HelloWorld(test.request)
+			assert.Equal(t, test.expected, result)
+		})
+	}
+}
+
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Jovansa")
 	if result != "Hello, Jovansa!" {
